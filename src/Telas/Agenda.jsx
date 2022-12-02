f242ce6fb05ab198544e15ebbff856c23f8a7548
Fillipe Silva <ss.fillipe@gmail.com>
2022-12-02 15:26:50 -0300

    Adding full calendar

--- src/Telas/Agenda.jsx
+++ src/Telas/Agenda.jsx
@@ -1,13 +1,44 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 const Agenda = () => {
   const [dates, setDates] = useState(new Date());
+  const events = [
+    {
+      id: 1,
+      title: 'event 1',
+      start: '2022-12-14T10:00:00',
+      end: '2022-12-14T12:00:00',
+    },
+    {
+      id: 2,
+      title: 'event 2',
+      start: '2022-12-16T13:00:00',
+      end: '2022-12-16T18:00:00',
+      color: "red"
+    },
+    { 
+      id: 3, 
+      title: 'event 3', 
+      start: '2022-12-17', 
+      end: '2022-12-20' },
+  ];
 
   return (
     <div>
-      <Calendar onChange={setDates} value={dates} />
+      <FullCalendar 
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+        }}
+        events={events}
+      />
     </div>
   );
 };
